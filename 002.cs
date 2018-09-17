ee6ed92ed7dd50789cd135de8716f90d91303489
leesungu <37622151+leesungu@users.noreply.github.com>
2018-09-17 11:16:10 +0900

    4444

--- 002.cs
+++
@@ -1,9 +0,0 @@
-public static string RemoveSpecialCharacters(this string str) {
-   StringBuilder sb = new StringBuilder();
-   foreach (char c in str) {
-      if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_') {
-         sb.Append(c);
-      }
-   }
-   return sb.ToString();
-}
